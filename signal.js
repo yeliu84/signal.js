5f021403d7b21e2b329ce8af3e4fe67216821b57
Ye Liu <yeliu@instast.com>
2015-04-02 21:19:25 -0400

    version 0.1.3: added bower.json

--- signal.js
+++ signal.js
@@ -2,7 +2,7 @@
     var prevSignal = global.Signal;
 
     var Signal = {
-        __version__: '0.1.2',
+        __version__: '0.1.3',
         __license__: 'MIT',
         __author__: 'Ye Liu',
         __contact__: 'yeliu@instast.com',
